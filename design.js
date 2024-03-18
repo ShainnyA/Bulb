eebb45565f7b867ce55de37a4bcb7487109951ea
ShainnyA <129950173+ShainnyA@users.noreply.github.com>
2024-03-18 20:12:26 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,10 @@
+function turnOn(){
+    var lightBulb = document.getElementById('offbulb');
+    lightBulb.src = './offbulb.png';
+}
+
+function turnOff(){
+
+    var lightBulb = document.getElementById('offbulb');
+    lightBulb.src = './onbulb.png';
+}
